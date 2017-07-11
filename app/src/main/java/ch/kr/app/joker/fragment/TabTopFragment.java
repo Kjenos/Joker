bcadcbbaabf8145b45ed00e9167e8dfc479709ec
tbricken <kevin.rickenbach@stud.hslu.ch>
2017-07-11 16:43:29 +0200

    created jokeFragment

--- app/src/main/java/ch/kr/app/joker/fragment/TabTopFragment.java
+++ app/src/main/java/ch/kr/app/joker/fragment/TabTopFragment.java
@@ -2,6 +2,8 @@ package ch.kr.app.joker.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +17,14 @@ import ch.kr.app.joker.R;
 public class TabTopFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        JokeFragment topJoke = new JokeFragment();
+        fragmentTransaction.add(R.id.topJokeFragmentContainer, topJoke, "HELLO");
+        fragmentTransaction.commit();
+
         return inflater.inflate(R.layout.tab_top_frag_layout, container, false);
     }
 }
